eff2cccfcd8e8098ac2a97b4b4c49a3e570c39ed
Pavel Berezhnoy <pberejnoy2005@gmail.com>
2015-08-18 21:13:16 +0000

    ApekEnergo: markup fixes

--- front/public/js/main.js
+++ front/public/js/main.js
@@ -158,5 +158,16 @@
   };
 })(jQuery);
 
+////////////////////////////////////////////////////////////////////////////tab 
 
+(function($) {
+$(function() {
+
+  $('ul.tabs').delegate('li:not(.current)', 'click', function() {
+    $(this).addClass('current').siblings().removeClass('current')
+      .parents('div.section').find('div.box').hide().eq($(this).index()).fadeIn(150);
+  })
+
+})
+})(jQuery)
 
